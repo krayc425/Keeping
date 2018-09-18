f3212bb5693e57630ba7bf0413474eaffc7b9df1
Kuixi Song <krayc425@gmail.com>
2018-09-19 00:46:20 +0800

    Init SiriKit

---
+++ Keeping/KeepingIntentExtension/IntentHandler.h
@@ -0,0 +1,13 @@
+//
+//  IntentHandler.h
+//  KeepingIntentExtension
+//
+//  Created by 宋 奎熹 on 2018/9/19.
+//  Copyright © 2018 宋 奎熹. All rights reserved.
+//
+
+#import <Intents/Intents.h>
+
+@interface IntentHandler : INExtension
+
+@end
---
+++ Keeping/KeepingIntentExtensionUI/IntentViewController.h
@@ -0,0 +1,13 @@
+//
+//  IntentViewController.h
+//  KeepingIntentExtensionUI
+//
+//  Created by 宋 奎熹 on 2018/9/19.
+//  Copyright © 2018 宋 奎熹. All rights reserved.
+//
+
+#import <IntentsUI/IntentsUI.h>
+
+@interface IntentViewController : UIViewController <INUIHostedViewControlling>
+
+@end
